0b6c7431c9d9c3262899511ccea46f2d314c7d88
Roberto <rvallejosbarria@gmail.com>
2021-04-18 21:34:19 -0400

    Files Shop and Product

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -1,9 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2021 robertov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package labs.pm.app;
+
+import java.math.BigDecimal;
+import labs.pm.data.Product;
 
 /**
  *
@@ -15,7 +29,12 @@ public class Shop {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Product p = new Product();
+        p.setId(101);
+        p.setName("Tea");
+        p.setPrice(BigDecimal.valueOf(1.99));
+        
+        System.out.println(p.getId() + " " + p.getName() + " " + p.getPrice());
     }
     
 }
--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -16,10 +16,38 @@
  */
 package labs.pm.data;
 
+import java.math.BigDecimal;
+
 /**
  *
  * @author robertov
  */
 public class Product {
-    
+    private int id;
+    private String name;
+    private BigDecimal price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
